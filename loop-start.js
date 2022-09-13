1528a4373220475487aba6df3583ae09c9067493
Mohammad Mustafijur Rahman <rahmanbond0@gmail.com>
2022-09-13 19:39:13 +0800

    first commit

---
+++ loop-start.js
@@ -0,0 +1,21 @@
+var number = 0;
+
+while (number < 9) {
+    console.log('no , no , no');
+    // console.log('number + 1')
+    console.log(number);
+    number++
+}
+
+
+var number = 0;
+
+while (number < 5) {
+    console.log(number);
+    // number = number + 2;
+    number++
+}
+
+for (var number = 1; number <= 8; number++){
+    console.log(number)
+}
